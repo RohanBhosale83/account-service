8f2876edd1ef4094346ed900d362b016edc8f1a6
Rohan Bhosale <rohan.bhosale2537@gmail.com>
2025-07-13 12:08:14 +0530

    updated changes

--- src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,19 +1,13 @@
 package com.eazybytes.accounts;
 
-import com.eazybytes.accounts.audit.AuditAwareImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(
-        { "com.eazybytes.accounts.entity", "com.eazybytes.accounts.model", "com.eazybytes.accounts.mappers.interfaces",
-                "com.eazybytes.accounts.service.interfaces", "com.eazybytes.accounts.controller.interfaces",
-                "com.eazybytes.accounts.controller.implementation", "com.eazybytes.accounts.repository.interfaces" })
+@EntityScan("com.eazybytes.accounts.entity")
 @EnableJpaRepositories("com.eazybytes.accounts.repository.interfaces")
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class AccountsApplication
